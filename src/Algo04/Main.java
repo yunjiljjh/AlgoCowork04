ad886df52f4f519e005052b916971def5510cf0c
yunjiljjh <yunjiljjh@naver.com>
2016-10-30 00:21:25 +0900

    .
    .

--- src/Algo04/Main.java
+++ src/Algo04/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		InputReader reader=new InputReader("C:\\Users\\samsung\\Documents\\GitHub\\AlgoCowork04");
+		System.out.println("run");
+		InputReader reader=new InputReader("C:\\hw4\\input.txt");
 		
 		//int a[][];
 	}
