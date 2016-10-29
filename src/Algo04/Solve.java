e059749e1af8bb4c6741b5bc66900c809ae76b20
yunjiljjh <yunjiljjh@naver.com>
2016-10-30 00:59:45 +0900

    .
    .

--- src/Algo04/Solve.java
+++ src/Algo04/Solve.java
@@ -1,5 +1,11 @@
 package Algo04;
 
+/* 
+ *  pattern types = total 8 patterns (_ is empty and # is pebble)
+ *  vertical patterns are laid horizontally for the sake of expression here.
+ *  pattern type 0: _ _ _ _ // 1: #_ _ _ // 2: _#_ _ // 3: _ _#_ // 4: _ _ _# // 5: #_#_ // 6: #_ _# // 7: _#_#
+ */
+
 public class Solve {
 
 	public Solve(){//constructer
@@ -9,6 +15,7 @@ public class Solve {
  * check if 'i'th column and 'k'th column are compatible
  */
 private boolean checkCompatible(int i, int k){ 
+
  return true;
 }
 	
